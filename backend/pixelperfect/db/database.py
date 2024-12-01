88423aa45a86074f1815861a2db0cf35e60f9cec
Joris Boelen <jboelen@tux-inside.com>
2024-12-01 14:51:21 +0100

    Test

--- backend/pixelperfect/db/database.py
+++ backend/pixelperfect/db/database.py
@@ -6,7 +6,7 @@ from sqlmodel import Session
 database_url = str(settings.sqlalchemy_database_url)
 database_connect_args = {"check_same_thread": False} if settings.DATABASE_SCHEME == 'sqlite' else {}
 
-engine = create_engine(url=database_url, connect_args=database_connect_args, pool_size=100, max_overflow=30)
+engine = create_engine(url=database_url, connect_args=database_connect_args, pool_size=200, max_overflow=50)
 
 Base = declarative_base()
 
